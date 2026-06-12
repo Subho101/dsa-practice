f214da340f631348693658781664247d51261f32
subhosmg@gmail.com <subhosmg@gmail.com>
2026-06-12 17:14:00 +0530

    Surround region

---
+++ src/test/java/com/subho/dsa/algos/SurroundRegionTest.java
@@ -0,0 +1,26 @@
+package com.subho.dsa.algos;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.subho.dsa.graph.algos.SurroundRegion;
+
+public class SurroundRegionTest {
+    
+    @Test
+    public void test1() {
+        char[][] board = {
+            {'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}
+        };
+
+        char[][] expected = {
+            {'X','X','X','X'},{'X','X','X','X'},{'X','X','X','X'},{'X','O','X','X'}
+        };
+
+        SurroundRegion.fillRegion(board);
+
+        assertArrayEquals(expected, board);
+    }
+
+}
